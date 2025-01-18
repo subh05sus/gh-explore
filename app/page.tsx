45edfdc21ac4f751f0b4f32312a4590259875bf1
subh05sus <sahasubhadipfkk@gmail.com>
2025-01-18 17:41:11 +0530

    Add vertical padding to main container in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center lg:p-24 md:p-16 p-8">
+    <main className="flex min-h-screen flex-col items-center justify-center lg:p-24 md:p-16 p-8 py-16">
       <RepoExplorer />
       <FloatingButton />
     </main>
